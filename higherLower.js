889f519326bb7012f0fe7873e98aa153bbb45653
Harsh Gajera <harsh.gajera17@gmail.com>
2022-03-18 17:51:23 +0530

    done

---
+++ higherLower.js
@@ -0,0 +1,411 @@
+data = [
+    {
+        name: 'Instagram',
+        follower_count: 395,
+        description: 'Social media platform',
+        country: 'United States'
+    },
+    {
+        name: 'Cristiano Ronaldo',
+        follower_count: 284,
+        description: 'Footballer',
+        country: 'Portugal'
+    },
+    {
+        name: 'Dwayne Johnson',
+        follower_count: 240,
+        description: 'Actor and professional wrestler',
+        country: 'United States'
+    },
+    {
+        name: 'Ariana Grande',
+        follower_count: 235,
+        description: 'Musician and actress',
+        country: 'United States'
+    },
+      {
+        name: 'Kylie Jenner',
+        follower_count: 230,
+        description: 'Reality TV personality and businesswoman and Self-Made Billionaire',
+        country: 'United States'
+    },
+    {
+        name: 'Selena Gomez',
+        follower_count: 224,
+        description: 'Musician and actress',
+        country: 'United States'
+    },
+  
+    {
+        name: 'Kim Kardashian',
+        follower_count: 217,
+        description: 'Reality TV personality and businesswoman',
+        country: 'United States'
+    },
+    {
+        name: 'Lionel Messi',
+        follower_count: 190,
+        description: 'Footballer',
+        country: 'Argentina'
+    },
+    {
+        name: 'Beyoncé',
+        follower_count: 175,
+        description: 'Musician',
+        country: 'United States'
+    },
+    {
+        name: 'Justin Bieber',
+        follower_count: 173,
+        description: 'Musician',
+        country: 'Canada'
+    }, {
+        name: 'Kendall Jenner',
+        follower_count: 162,
+        description: 'Reality TV personality and Model',
+        country: 'United States'
+    },
+ {
+        name: 'National Geographic',
+        follower_count: 158,
+        description: 'Magazine',
+        country: 'United States'
+    },
+        {
+        name: 'Taylor Swift',
+        follower_count: 154,
+        description: 'Musician',
+        country: 'United States'
+    },
+   
+    {
+        name: 'Jennifer Lopez',
+        follower_count: 153,
+        description: 'Musician and actress',
+        country: 'United States'
+    },
+    {
+        name: 'Neymar',
+        follower_count: 151,
+        description: 'Footbowler',
+        country: 'Brazil'
+    },
+    {
+        name: 'Nicki Minaj',
+        follower_count: 140,
+        description: 'Musician',
+        country: 'Trinidad and Tobago'
+    },
+    {
+        name: 'Nike',
+        follower_count: 130,
+        description: 'Sportswear multinational',
+        country: 'United States'
+    },
+    {
+        name: 'Khloé Kardashian',
+        follower_count: 125,
+        description: 'Reality TV personality and businesswoman',
+        country: 'United States'
+    },
+    {
+        name: 'Miley Cyrus',
+        follower_count: 107,
+        description: 'Musician and actress',
+        country: 'United States'
+    },
+    {
+        name: 'Katy Perry',
+        follower_count: 94,
+        description: 'Musician',
+        country: 'United States'
+    },
+    {
+        name: 'Kourtney Kardashian',
+        follower_count: 90,
+        description: 'Reality TV personality',
+        country: 'United States'
+    },
+    {
+        name: 'Kevin Hart',
+        follower_count: 89,
+        description: 'Comedian and actor',
+        country: 'United States'
+    },
+    {
+        name: 'Ellen DeGeneres',
+        follower_count: 87,
+        description: 'Comedian',
+        country: 'United States'
+    },
+    {
+        name: 'Real Madrid CF',
+        follower_count: 86,
+        description: 'Football club',
+        country: 'Spain'
+    },
+    {
+        name: 'FC Barcelona',
+        follower_count: 85,
+        description: 'Football club',
+        country: 'Spain'
+    },
+    {
+        name: 'Rihanna',
+        follower_count: 81,
+        description: 'Musician and businesswoman',
+        country: 'Barbados'
+    },
+    {
+        name: 'Demi Lovato',
+        follower_count: 80,
+        description: 'Musician and actress',
+        country: 'United States'
+    },
+    {
+        name: "Victoria's Secret",
+        follower_count: 69,
+        description: 'Lingerie brand',
+        country: 'United States'
+    },
+    {
+        name: 'Zendaya',
+        follower_count: 68,
+        description: 'Actress and musician',
+        country: 'United States'
+    },
+    {
+        name: 'Shakira',
+        follower_count: 66,
+        description: 'Musician',
+        country: 'Colombia'
+    },
+    {
+        name: 'Drake',
+        follower_count: 65,
+        description: 'Musician',
+        country: 'Canada'
+    },
+    {
+        name: 'Chris Brown',
+        follower_count: 64,
+        description: 'Musician',
+        country: 'United States'
+    },
+    {
+        name: 'LeBron James',
+        follower_count: 63,
+        description: 'Basketball player',
+        country: 'United States'
+    },
+    {
+        name: 'Vin Diesel',
+        follower_count: 62,
+        description: 'Actor',
+        country: 'United States'
+    },
+    {
+        name: 'Cardi B',
+        follower_count: 67,
+        description: 'Musician',
+        country: 'United States'
+    },
+    {
+        name: 'David Beckham',
+        follower_count: 82,
+        description: 'Footballer',
+        country: 'United Kingdom'
+    },
+    {
+        name: 'Billie Eilish',
+        follower_count: 61,
+        description: 'Musician',
+        country: 'United States'
+    },
+    {
+        name: 'Justin Timberlake',
+        follower_count: 59,
+        description: 'Musician and actor',
+        country: 'United States'
+    },
+    {
+        name: 'UEFA Champions League',
+        follower_count: 58,
+        description: 'Club football competition',
+        country: 'Europe'
+    },
+    {
+        name: 'NASA',
+        follower_count: 56,
+        description: 'Space agency',
+        country: 'United States'
+    },
+    {
+        name: 'Emma Watson',
+        follower_count: 56,
+        description: 'Actress',
+        country: 'United Kingdom'
+    },
+    {
+        name: 'Shawn Mendes',
+        follower_count: 57,
+        description: 'Musician',
+        country: 'Canada'
+    },
+    {
+        name: 'Virat Kohli',
+        follower_count: 55,
+        description: 'Cricketer',
+        country: 'India'
+    },
+    {
+        name: 'Gigi Hadid',
+        follower_count: 54,
+        description: 'Model',
+        country: 'United States'
+    },
+    {
+        name: 'Priyanka Chopra Jonas',
+        follower_count: 53,
+        description: 'Actress and musician',
+        country: 'India'
+    },
+    {
+        name: '9GAG',
+        follower_count: 52,
+        description: 'Social media platform',
+        country: 'China'
+    },
+    {
+        name: 'Ronaldinho',
+        follower_count: 51,
+        description: 'Footballer',
+        country: 'Brasil'
+    },
+    {
+        name: 'Maluma',
+        follower_count: 50,
+        description: 'Musician',
+        country: 'Colombia'
+    },
+    {
+        name: 'Camila Cabello',
+        follower_count: 49,
+        description: 'Musician',
+        country: 'Cuba'
+    },
+    {
+        name: 'NBA',
+        follower_count: 47,
+        description: 'Club Basketball Competition',
+        country: 'United States'
+    }
+]
+let aname=document.getElementById("aname")
+let afollow=document.getElementById("afollow")
+let adescr=document.getElementById("adescr")
+let acount = document.getElementById("acount")
+
+let bname=document.getElementById("bname")
+let bfollow=document.getElementById("bfollow")
+let bdescr=document.getElementById("bdescr")
+let bcount = document.getElementById("bcount")
+
+a = random()
+b = random()
+if (a == b && b !== 50) a = b + 1
+else a=b-1
+aname.innerHTML=`<b>Name: </b>${data[a].name}`
+afollow.innerHTML=`<b>Followers: </b>${data[a].follower_count} millions`
+adescr.innerHTML=`<b>Description: </b>${data[a].description}`
+acount.innerHTML=`<b>Country: </b>${data[a].country}`
+
+bname.innerHTML=`<b>Name: </b>${data[b].name}`
+bfollow.innerHTML=`<b>Followers: </b>? millions`
+bdescr.innerHTML=`<b>Description: </b>${data[b].description}`
+bcount.innerHTML=`<b>Country: </b>${data[b].country}`
+
+let hBtn = document.getElementById("high")
+let lBtn = document.getElementById("low")
+let h2=document.querySelector("h2")
+score=0
+hBtn.addEventListener("click", function () {
+
+    if (data[b].follower_count >= data[a].follower_count) {
+        // alert("Yes")
+        score++
+
+        bfollow.innerHTML = `<b>Followers: </b>${data[b].follower_count} millions`
+
+        bfollow.innerHTML = `<b>Followers: </b>${data[b].follower_count} millions`
+        c = random()
+        aname.innerHTML=`<b>Name: </b>${data[b].name}`
+        afollow.innerHTML=`<b>Followers: </b>${data[b].follower_count} millions`
+        adescr.innerHTML=`<b>Description: </b>${data[b].description}`
+        acount.innerHTML=`<b>Country: </b>${data[b].country}`
+        
+        bname.innerHTML=`<b>Name: </b>${data[c].name}`
+        bfollow.innerHTML=`<b>Followers: </b>? millions`
+        bdescr.innerHTML=`<b>Description: </b>${data[c].description}`
+        bcount.innerHTML = `<b>Country: </b>${data[c].country}`
+        
+        console.log(data[b].follower_count)
+        console.log(data[a].follower_count)
+        data[a].follower_count=data[b].follower_count
+
+        data[b].name=data[c].name
+        data[b].follower_count=data[c].follower_count
+        data[b].description=data[c].description
+        data[b].country = data[c].country
+        
+        
+h2.innerHTML=`Your Total Score is <span>${score}</span>.`
+    }
+     else {
+        alert("You Loss Game!!!!!!!!!!!")
+        score = 0
+                
+h2.innerHTML=`Your Total Score is <span>${score}</span>.`
+    }
+})
+
+lBtn.addEventListener("click", function () {
+    if (data[b].follower_count <= data[a].follower_count) {
+        // alert("Yes")
+        score++
+        c = random()
+       
+        aname.innerHTML=`<b>Name: </b>${data[b].name}`
+        afollow.innerHTML=`<b>Followers: </b>${data[b].follower_count} millions`
+        adescr.innerHTML=`<b>Description: </b>${data[b].description}`
+        acount.innerHTML=`<b>Country: </b>${data[b].country}`
+        
+        bname.innerHTML=`<b>Name: </b>${data[c].name}`
+        bfollow.innerHTML=`<b>Followers: </b>? millions`
+        bdescr.innerHTML=`<b>Description: </b>${data[c].description}`
+        bcount.innerHTML = `<b>Country: </b>${data[c].country}`
+        
+        console.log(data[b].follower_count)
+        console.log(data[a].follower_count)
+        data[a].follower_count=data[b].follower_count
+
+        data[b].name=data[c].name
+        data[b].follower_count=data[c].follower_count
+        data[b].description=data[c].description
+        data[b].country = data[c].country
+        
+        h2.innerHTML=`Your Total Score is <span>${score}</span>.`
+    }
+    else {
+        alert("You Loss Game!!!!!!!!!!!")
+        score = 0
+                
+h2.innerHTML=`Your Total Score is <span>${score}</span>.`
+    }
+})
+
+function random() {
+    r = Math.floor(Math.random()*50)
+    return r
+}
+random(1)
